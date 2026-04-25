f34c7b27cacecfa1a4e1d11c92a59363d31bc311
Aditya Gupta <adi1998.2010@gmail.com>
2026-04-25 09:58:11 +0530

    cleanup

--- build.ps1
+++
@@ -1,3 +0,0 @@
-python build_pkg.py -s .\textures\GR2 -t .\data\GR2
-
-mv .\data\GR2.pkg .\data\AxeTest-WeaponAxe.pkg -force
--- src/def.lua
+++ src/def.lua
@@ -1,4 +1,4 @@
----@meta zerp-MelSKin
+---@meta zerp-WeaponFusion
 local public = {}
 
 -- document whatever you made publicly available to other plugins here
@@ -8,9 +8,5 @@ local public = {}
 --    ---@param b string helpful description
 --    ---@return table c helpful description
 --    function public.do_stuff(a, b) end
-
----@param dressdata table DressData containing GrannyTexture field, other fields are not supported
----@param packages table list of package/s for the skin/s
-function public.AddEntriesToDressData(dressdata, packages) end
 
 return public
