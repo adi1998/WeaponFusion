f414741eca19e963b3c7c730e25595a6bab46a66
Aditya Gupta <adi1998.2010@gmail.com>
2026-05-05 01:57:08 +0530

    add more HeroHasTrait context wraps

--- src/ready_late.lua
+++ src/ready_late.lua
@@ -21,11 +21,33 @@ modutil.mod.Path.Context.Wrap.Static("CheckFinisher",function (victim, functionA
 	end)
 end)
 
+function HeroHasTraitSuitComboAspectWrap(base, traitName)
+	if traitName == "SuitComboAspect" then
+		return base(traitName) or base(traitName.."_Secondary")
+	end
+	return base(traitName)
+end
+
 modutil.mod.Path.Context.Wrap.Static("CheckDifferentOmegaCrit", function ( weaponData, functionArgs, triggerArgs )
 	modutil.mod.Path.Wrap("HeroHasTrait", function (base, traitName)
-		if traitName == "SuitComboAspect" then
-			return base(traitName) or base(traitName.."_Secondary")
-		end
-		return base(traitName)
+		return HeroHasTraitSuitComboAspectWrap(base, traitName)
+	end)
+end)
+
+modutil.mod.Path.Context.Wrap.Static("CheckSprintBonusVolley", function ( weaponData, functionArgs, triggerArgs )
+	modutil.mod.Path.Wrap("HeroHasTrait", function (base, traitName)
+		return HeroHasTraitSuitComboAspectWrap(base, traitName)
+	end)
+end)
+
+modutil.mod.Path.Context.Wrap.Static("CheckDrinkCritCharges", function ( weaponData, functionArgs, triggerArgs )
+	modutil.mod.Path.Wrap("HeroHasTrait", function (base, traitName)
+		return HeroHasTraitSuitComboAspectWrap(base, traitName)
+	end)
+end)
+
+modutil.mod.Path.Context.Wrap.Static("CheckLeapCritCharges", function ( weaponData, functionArgs, triggerArgs )
+	modutil.mod.Path.Wrap("HeroHasTrait", function (base, traitName)
+		return HeroHasTraitSuitComboAspectWrap(base, traitName)
 	end)
 end)
